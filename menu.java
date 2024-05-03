3bb45aebdba6ff280756e8531441326f3d7b66b4
Danimart22 <danimartinez12@live.com>
2024-05-03 07:39:19 -0500

    Agregue elementos al menu (La lista de pacientes) y modifique el metodo nuevo paciente y agregue el metodo mostrar paciente

--- menu.java
+++ menu.java
@@ -1,10 +1,12 @@
 package PracticaSalud;
 
 import javax.swing.JOptionPane;
-
+import java.util.LinkedList;
 public class menu {
     public static void Menu(){
         int decision = 0;
+        LinkedList<Paciente> listaPacientes = new LinkedList<>();
+        listaPacientes = metodos.Nuevo_Paciente(listaPacientes);
 
         do{
             decision = Integer.parseInt(JOptionPane.showInputDialog("\n---MENU DEL PROGRAMA DE PACIENTES Y ASIGNACIÓN DE CITAS---\n\n1. Agregar un nuevo paciente\n2. Mostrar todos los pacientes registrados\n0. Para salir\n ELija unna opción: "));
--- metodos.java
+++ metodos.java
@@ -1,8 +1,29 @@
 package PracticaSalud;
 import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
 public class metodos {
     public static LinkedList<Paciente> Nuevo_Paciente(LinkedList<Paciente> pacientes){
+        String decision = "";
+        
+        do{
+            Paciente paciente_nuevo = new Paciente(null, null, 0, null);
+            paciente_nuevo.setNombre(JOptionPane.showInputDialog("\nIngrese el nombre del paciente: "));
+            paciente_nuevo.setApellido(JOptionPane.showInputDialog("\nIngrese el apellido del paciente: ")); 
+            paciente_nuevo.setEdad(Integer.parseInt(JOptionPane.showInputDialog("\nIngrese la edad del paciente: ")));
+            paciente_nuevo.setRH(JOptionPane.showInputDialog("\nIngrese el tipo de sangre (RH) del paciente: "));
+
+    
+        }while(decision.equals("s"));
         
         return pacientes;
+    }
+    public static String Mostrar_Paciente(LinkedList<Paciente> pacientes){
+        String cadena = "";
+        for (Paciente paciente : pacientes) {
+            cadena = paciente.getNombre() + "\n"+ paciente.getApellido()+"\n"+paciente.getEdad()+"\n"+paciente.getRH();
+        }
+        return cadena;
+
     }
 }
